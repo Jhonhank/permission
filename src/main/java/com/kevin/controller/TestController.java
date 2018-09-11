bc899cd97284f4beabe2740f6e7cb9c81171155e
Jhonhank <1747956669@qq.com>
2018-09-11 10:18:39 +0800

    three commit

---
+++ src/main/java/com/kevin/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.kevin.controller;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        log.info("hello");
+        return "hello,permission";
+    }
+}
---
+++ src/main/java/com/kevin/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.kevin.dao;
+
+public interface UserDao {
+}
